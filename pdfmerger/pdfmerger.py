a1cd35fb93d2267375a5af29d65c3edced0d8066
Jim Holmström <jim.holmstroem@gmail.com>
2013-02-18 22:49:59 +0100

    works

--- pdfmerger/pdfmerger.py
+++ pdfmerger/pdfmerger.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from __future__ import print_function
 import cgi
 import time
@@ -8,73 +7,27 @@ import BaseHTTPServer as httpserver
 from functools import partial, reduce
 import operator as op
 
-HOSTNAME = '192.168.1.85'
-PORT = 80
+#import ghostscript #really buggy didn't get it to work properly.. NOTE you have to have ghostscript installed
+from subprocess import call
+from tempfile import NamedTemporaryFile
+
+HOSTNAME, PORT = '192.168.1.85', 80
 
 def unpack(f, a):
     return f(*a)
 def unpackd(f,a):
     return f(**a)
-
 def tee(a):
     print(a)
     return a
 
-padding = partial(op.add, " "*4)
-
-def tag(name, *content, **attributes):
-    return "<{name}{attributes}>\n{content}\n</{name}>".format(
-        name=name,
-        content='\n'.join(content),
-        attributes=reduce(op.add,
-            map(
-                partial(
-                    unpack,
-                    ' {0}="{1}"'.format
-                ),
-                attributes.items()
-            ),
-            ""
-        )
-    )
-
-#class Path(object):
-#    def __init__(self, m):
-#        self.directory, *variablestring = m.split('?')
-#        self.folders = list(filter(bool, self.directory.split('/')))
-#        if(variablestring):
-#            def parse_variable(v):
-#                return tuple(v.split("="))
-#            self.variables = dict(
-#                map(
-#                    parse_variable,
-#                    variablestring[0].split("&")
-#                )
-#            )
-#        else:
-#            self.variables = []
-#
-#    def __repr__(self):
-#        return "Path(folders={folders}, variables={variables})".format(folders=self.folders, variables=self.variables)
- 
 class Handler(httpserver.BaseHTTPRequestHandler):
-    def do_HEAD(s):
-        s.send_response(200)
-        s.send_header("Content-Type", "application/pdf")
-        s.end_headers()
-
     def do_GET(self):
+        print("GET")
         self.send_response(200)
-        self.send_header("Content-type", "text/plain")
+        self.send_header('Content-type','text/plain')
         self.end_headers()
-        print("GET") 
-        self.wfile.write("<html>GET")
-        return       
-        with open('../data/example.pdf','r') as f:
-	    for l in list(f):
-                self.wfile.write(l)
-        return 
-
+        self.wfile.write('GET')
     def do_POST(self):
         form = cgi.FieldStorage(
             fp=self.rfile,
@@ -84,109 +37,84 @@ class Handler(httpserver.BaseHTTPRequestHandler):
                 'CONTENT_TYPE':self.headers['Content-Type'],
             }
         )
-	# Begin the response
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        print(type(self.wfile))
-        self.wfile.write("HELLO WORLD")
-        print("HELLO WORLD")
         
-	self.wfile.write('Client: %s\n' % str(self.client_address))
-	print('Client: %s' % str(self.client_address))
-        self.wfile.write('User-agent: %s\n' % str(self.headers['user-agent']))
-        print('User-agent: %s' % str(self.headers['user-agent']))
-        self.wfile.write('Path: %s\n' % self.path)
-        print('Path: %s' % self.path)
-        self.wfile.write('Form data:\n')
+        pdf_properties = dict(
+            delete=False, 
+            suffix='.pdf'
+        )
+        output_pdf_properties = dict(
+            pdf_properties,
+            **dict(
+                mode='r',
+            )
+        )
 
-        # Echo back information about what was posted in the form
-        for field in form.keys():
-            field_item = form[field]
-	    if field_item.filename:
-	        print(field_item)
-	        # The field contains an uploaded file
-	        file_data = field_item.file.read()
-	        file_len = len(file_data)
-	        del file_data
-	        self.wfile.write('\tUploaded %s as "%s" (%d bytes)\n' % (field, field_item.filename, file_len))
-	        print('Uploaded %s as "%s" (%d bytes)' % (field, field_item.filename, file_len))
+        try:
+            def prepare_pdf_file(field_item):
+                file_data = field_item.file.read()
+                temp_file = NamedTemporaryFile(**pdf_properties)
+                temp_file.write(file_data)
+                temp_file.flush()
+                print('Uploaded "{filename}" ({size} bytes)'.format( filename=field_item.filename, size=len(file_data)))
+                return temp_file
+            
+            def is_file(field_item):
+                return bool(field_item.filename)
 
-        print("DONE")
-        return
-#    def do_GET(s):
-#        """Responde to a GET request"""
-#
-#        def write(m, encoding="utf-8"):
-#            s.wfile.write(
-#                bytes(
-#                    m, 
-#                    encoding=encoding
-#                )
-#            )
-#        
-#        def render_item(item):
-#            def CDATA(data):
-#                return "<![CDATA[{data}]]>".format(data=data)
-#            return tag("item",
-#                *map(
-#                    lambda name: tag(
-#                        name, 
-#                        padding(
-#                            CDATA(
-#                                item[name]
-#                            )
-#                        )
-#                    ),
-#                    filter(
-#                        lambda t: not t.startswith('_'),
-#                        item
-#                    )
-#                )
-#            )
-#
-#        path = Path(s.path)
-#        print(path)
-#        if(len(path.folders)>0 and path.folders[0]=='favicon.ico' or not len(path.folders)):
-#            s.send_response(404)
-#            s.end_headers()
-#            return
-#
-#        rss_id = path.folders[0]
-#        
-#        s.send_response(200)
-#        s.send_header("Content-type", "text/xml")
-#        s.end_headers()
-#        
-#        database = pymongo.MongoClient('localhost', 1337)['rss-database']
-#        collection = database[rss_id]
-#
-#        write('<?xml version="1.0" encoding="UTF-8"?>\n')
-#        write(
-#            tag("rss",
-#                tag("channel",
-#                    tag("title", "{rss_id} RSS".format(rss_id=rss_id)),
-#                    tag("link", "{domain}/{rss_id}".format(domain=HOSTNAME, rss_id=rss_id)),
-#                    tag("description", "RSS feed for {rss_id}".format(rss_id=rss_id)),
-#                    tag("language", "en-us"),
-#                    tag("copyright", "Copyright 2013, Jim Holmström"),
-#                    tag("managingEditor", "me@jim.pm (Jim Holmström)"),
-#                    tag("webMaster", "me@jim.pm (Jim Holmström)"),
-#                    *map(                
-#                        render_item,
-#                        collection.find().sort('_id') #title/link/description/..
-#                    )
-#                ),
-#            version="2.0"
-#            )
-#        )
-
-#db=pymongo.MongoClient('localhost', 1337)['rss-database']
-#c=db['SE1055'] 
-#a=['1.1','1.2',..]
-#d=list(map(lambda t: {'title':t},a))
-#c.insert(d)
+            temp_input = map(
+                prepare_pdf_file,
+                filter(
+                    is_file,
+                    dict(form).values()
+                )
+            )
+            if(temp_input<1):
+                self.send_response(404)
+                self.end_headers()        
+            
+            #gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=finished.pdf file1.pdf file2.pdf
+            temp_output = NamedTemporaryFile(**output_pdf_properties)
+            command = [
+                "gs",
+                "-dBATCH", 
+                "-dNOPAUSE", 
+                "-q",
+                "-sDEVICE=pdfwrite", 
+                "-sOutputFile={filename}".format(
+                    filename=temp_output.name
+                )
+            ]+\
+            map(
+                op.attrgetter('name'), 
+                temp_input
+            )
+            print(' '.join(command)) 
+            call(command) #pretty safe since all inputs to the command string is temp filenames (no ``filename injection'' possible) #NOTE waits until returning
+        
+            self.send_response(200)
+            self.send_header("Content-type", "application/pdf")
+            self.end_headers()
+            
+            self.wfile.write(temp_output.read())
  
+        except Exception as e:
+            print("Error:", e)
+            self.send_response(500)
+            self.end_headers()
+        finally: #NOTE all the try-except's inside finally is to guard of not set variables
+            try:
+                temp_output.close()
+            except:
+                pass
+            try: 
+                for f in pdf_files: #temporary files are deleted after they are closed so beware, cannot use ``with'' in this case
+                    try:
+                        f.close()
+                    except:
+                        pass
+            except:
+                pass
+
 if __name__ == "__main__":
     httpd = httpserver.HTTPServer((HOSTNAME, PORT), Handler)
     print(time.asctime(), "Server start - {name}:{port}".format(name=HOSTNAME, port=PORT))
